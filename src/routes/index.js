25b02f15a331976f043483ca46abeb1e31017021
Manuel Barrios <mbarrios@MBP-de-Manuel.fibertel.com.ar>
2022-11-09 17:49:41 -0300

    icons

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
-import { Text, View,TextInput, TouchableOpacity } from 'react-native';
+import { Text, View,TextInput, TouchableOpacity,SafeAreaView,FlatList, Image } from 'react-native';
 
 
 const Stack = createNativeStackNavigator();
@@ -16,7 +16,7 @@ const MyTheme = {
     ...DefaultTheme,
     colors: {
       ...DefaultTheme.colors,
-      primary: 'rgb(255, 45, 85)',
+      primary: 'rgb(237, 28, 46)',
       background: 'rgb(242, 242, 242)',
       card: 'rgb(255, 255, 255)',
       text: 'rgb(28, 28, 30)',
@@ -43,10 +43,50 @@ const MyTheme = {
 function MyTabs() {
     return (
       <Tab.Navigator>
-        <Tab.Screen name="Rostro" component={HomeScreen} />
-        <Tab.Screen name="Cuerpo" component={SettingsScreen} />
-        <Tab.Screen name="Solares" component={SettingsScreen} />
-        <Tab.Screen name="Maquillaje" component={SettingsScreen} />
+        <Tab.Screen name="Rostro" component={HomeScreen} 
+        options={{
+        tabBarLabelStyle:{fontSize:10},
+        tabBarIcon: ({color}) => (
+            <Image
+              style={{ width: 28, height: 28 }}
+              source={require('../assets/img/icons/rostro_icon.png')}
+            />
+          )
+        }}
+       />
+        <Tab.Screen name="Cuerpo" component={SettingsScreen} 
+        options={{
+            tabBarLabelStyle:{fontSize:10},
+            tabBarIcon: ({color}) => (
+                <Image
+                  style={{ width: 28, height: 28 }}
+                  source={require('../assets/img/icons/cuerpo_icon.png')}
+                />
+              )
+            }}
+        />
+        <Tab.Screen name="Solares" component={SettingsScreen}
+        options={{
+            tabBarLabelStyle:{fontSize:10},
+            tabBarIcon: ({color}) => (
+                <Image
+                  style={{ width: 28, height: 28 }}
+                  source={require('../assets/img/icons/solar_icon.png')}
+                />
+              )
+            }}
+        /> 
+        <Tab.Screen name="Maquillaje" component={SettingsScreen}
+        options={{
+            tabBarLabelStyle:{fontSize:10},
+            tabBarIcon: ({color}) => (
+                <Image
+                  style={{ width: 28, height: 28 }}
+                  source={require('../assets/img/icons/maquillaje_icon.png')}
+                />
+              )
+            }}
+        />
 
 
       </Tab.Navigator>
@@ -55,10 +95,66 @@ function MyTabs() {
 
 function HomeScreen() {
     const { colors } = useTheme();
+
+    const DATA = [
+        {
+          id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+          title: 'First Item',
+        },
+        {
+          id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+          title: 'Second Item',
+        },
+        {
+          id: '58694a0f-3da1-471f-bd96-145571e29d72',
+          title: 'Third Item',
+        },
+        {
+            id: '58694a0f-3da1-471f-bd96-145571e29d72',
+            title: 'Third Item',
+        },
+        {
+            id: '58694a0f-3da1-471f-bd96-145571e29d72',
+            title: 'Third Item',
+        },
+        {
+            id: '58694a0f-3da1-471f-bd96-145571e29d72',
+            title: 'Third Item',
+        },
+        {
+            id: '58694a0f-3da1-471f-bd96-145571e29d72',
+            title: 'Third Item',
+        }
+      ];
+
+    const card = ({}) => {
+        return(
+            <View style={{ flexDirection: "row",  margin : 10, backgroundColor:colors.card  }} >
+                <View  style={{justifyContent:'center'}}>
+                    <Image
+                            style={{resizeMode:'cover',height:100, width:120}}
+                            source={require('../assets/rostro.png')}
+                    />
+                </View>
+                <View style={{ height : 120,  margin: 10 , flex: 1 }}>
+                    <View >
+                        <Text style={{color:colors.primary, fontWeight:'bold'}}>BIOSMOTIC WATER DROPS</Text>
+                    </View>
+                    <View style={{paddingTop: 20}}>
+                        <Text>Gel fluido de textura sedosa con un innovador activo biotecnológico bifuncional osmopretector, que brinda hidratación instantánea y prolongada.</Text>  
+                    </View> 
+                </View> 
+            </View>
+        )
+    }
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'  }}>
-        <Text>Home Screen</Text>
-      </View>
+       <SafeAreaView >
+            <FlatList
+                data={DATA}
+                renderItem={card}
+                keyExtractor={item => item.id}
+            />
+        </SafeAreaView>
     );
   }
 
